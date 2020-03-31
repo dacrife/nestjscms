b09c1d9608142db8363fcf8924202f47c7fd6658
dacrife <crosthian99@gmail.com>
2020-03-31 15:02:20 -0500

    database provider update

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,11 +5,8 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: async () =>
       await createConnection({
-        type: 'mysql',
+        type: 'mongodb',
         host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
         database: 'test',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
